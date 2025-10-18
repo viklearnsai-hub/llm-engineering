54ecf496c2491b2546da881bde3b271eda858b4a
Vikram <vik.learns.ai@gmail.com>
2025-10-18 14:22:54 +0530

    Lints and gitignore changes

--- src/day_one/first_day.py
+++ src/day_one/first_day.py
@@ -1,20 +1,19 @@
 import os
 from dotenv import load_dotenv
-from website import Website
-from llmClient import LLMClient
+from day_one.llm_client import LLMClient
+from day_one.website import Website
 
 load_dotenv()
 api_key = os.getenv("OPEN_AI_API_KEY")
 
 
-system_prompt = """You are a AI Expert who has indept understanding of the conepts in AI and you are also 
+SYSTEM_PROMPT = """You are a AI Expert who has indept understanding of the conepts in AI and you are also
                    an excelent teacher who can explain the concept in simple terms without skipping over any of the concept,
                    ignoring texts that might be related to navigation
-
                 """
 web = Website(
     "https://medium.com/@amanatulla1606/transformer-architecture-explained-2c49e2257b4c"
 )
 web.webscrape()
-client = LLMClient(system_prompt, api_key)
+client = LLMClient(SYSTEM_PROMPT, api_key)
 print(client.summarize(web))
--- src/day_one/llm_client.py
+++ src/day_one/llm_client.py
@@ -12,24 +12,24 @@ class LLMClient:
         self.__token = token
         self.client = OpenAI(api_key=self.__token)
 
-    def __setUserPrompt(self, website):
+    def __set_user_prompt(self, website):
         self.user_prompt = f"""You are looking at a website titled {website.title}.
         The content of the website is as follows.
-        Please explain the contents to me in simple yet effective way 
+        Please explain the contents to me in simple yet effective way
 
         {website.text}"""
 
-    def __getMessagesObj(self):
+    def __get_messages_obj(self):
         return [
             {"role": "system", "content": self.system_prompt},
             {"role": "user", "content": self.user_prompt},
         ]
 
     def summarize(self, website):
-        self.__setUserPrompt(website)
-        msgs = self.__getMessagesObj()
+        self.__set_user_prompt(website)
+        msgs = self.__get_messages_obj()
         response = self.client.chat.completions.create(
             model="gpt-4.1-mini", messages=msgs
         )
-        responseTxt = response.choices[0].message.content
-        return responseTxt
+        response_txt = response.choices[0].message.content
+        return response_txt
